e7193d8d54945fb5ceedba3b6a0702ab27fc7c2b
Jake Shelby <jake.shelby@me.com>
2015-03-15 10:31:41 -0600

    Added cors header

--- cmd/rpc-server/main.go
+++ cmd/rpc-server/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"strings"
 
 	"github.com/gorilla/rpc/v2"
 	"github.com/gorilla/rpc/v2/json2"
@@ -13,7 +14,7 @@ import (
 func main() {
 	s := rpc.NewServer()
 	// json-rpc version 2
-	s.RegisterCodec(json2.NewCodec(), "application/json")
+	s.RegisterCodec(CodecWithCors([]string{"*"}, json2.NewCodec()), "application/json")
 	s.RegisterService(new(WebService), "PhotoChem")
 	http.Handle("/rpc", s)
 	http.ListenAndServe(":10000", nil)
@@ -140,4 +141,41 @@ func getListings(limit int, skip []string) ([]HouseListing, int) {
 	query.All(&result)
 
 	return result, count
+}
+
+func CodecWithCors(corsDomains []string, unpimped rpc.Codec) rpc.Codec {
+	return corsCodec{corsDomains, unpimped}
+}
+
+type corsCodecRequest struct {
+	corsDomains            []string
+	underlyingCodecRequest rpc.CodecRequest
+}
+
+//override exactly one method of the underlying anonymous field and delegate to it.
+func (ccr corsCodecRequest) WriteResponse(w http.ResponseWriter, reply interface{}) {
+	w.Header().Add("Access-Control-Allow-Origin", strings.Join(ccr.corsDomains, " "))
+	ccr.underlyingCodecRequest.WriteResponse(w, reply)
+}
+
+func (ccr corsCodecRequest) Method() (string, error) {
+	return ccr.underlyingCodecRequest.Method()
+}
+
+func (ccr corsCodecRequest) ReadRequest(request interface{}) error {
+	return ccr.underlyingCodecRequest.ReadRequest(request)
+}
+
+func (ccr corsCodecRequest) WriteError(w http.ResponseWriter, status int, err error) {
+	ccr.underlyingCodecRequest.WriteError(w, status, err)
+}
+
+type corsCodec struct {
+	corsDomains     []string
+	underlyingCodec rpc.Codec
+}
+
+//override exactly one method of the underlying anonymous field and delegate to it.
+func (cc corsCodec) NewRequest(req *http.Request) rpc.CodecRequest {
+	return corsCodecRequest{cc.corsDomains, cc.underlyingCodec.NewRequest(req)}
 }
