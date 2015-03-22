525ec8a1aea7aa82b9d7a4bcb6224f920b17f3a3
Jake Shelby <jake.shelby@me.com>
2015-03-22 17:45:24 -0600

    Updated the update crawler to use the new home library scraping functions
    Also added concurrency control; new cmd line params

--- cmd/update-crawler/main.go
+++ cmd/update-crawler/main.go
@@ -6,21 +6,26 @@ import (
 	"flag"
 	"fmt"
 	"io"
+	"net"
 	"net/http"
 	"os"
 	"strconv"
+	"strings"
+	"sync"
 	"time"
 
-	_ "github.com/PuerkitoBio/goquery"
 	"github.com/jmshelby/photochem/home"
 )
 
 const (
-	MarkupExpirationDays   = 7
 	DefaultNumberOfWorkers = 3
 )
 
+var GlobalWG sync.WaitGroup
+
 func main() {
+
+	fmt.Printf("Started - %v\n", time.Now())
 
 	flag.Parse()
 	args := flag.Args()
@@ -34,14 +39,37 @@ func main() {
 		fmt.Println("Please specify db name")
 		os.Exit(1)
 	}
+	if len(args) < 3 {
+		fmt.Println("Please specify stale days")
+		os.Exit(1)
+	}
+	if len(args) < 4 {
+		fmt.Println("Please specify batch amount")
+		os.Exit(1)
+	}
 
 	dbHost := args[0]
 	dbName := args[1]
 
+	var staleDays int
+	staleDays, convErr := strconv.Atoi(args[2])
+	if convErr != nil {
+		fmt.Printf("Bad number for stale days param")
+		os.Exit(2)
+	}
+	staleDate := time.Now().AddDate(0, 0, -1*staleDays)
+
+	var batchAmount int
+	batchAmount, convErr = strconv.Atoi(args[3])
+	if convErr != nil {
+		fmt.Printf("Bad number for batch amount param")
+		os.Exit(2)
+	}
+
 	var numberOfWorkers int
-	if len(args) > 2 {
+	if len(args) > 4 {
 		var convErr error
-		numberOfWorkers, convErr = strconv.Atoi(args[2])
+		numberOfWorkers, convErr = strconv.Atoi(args[4])
 		if convErr != nil {
 			fmt.Printf("Bad number of workers param")
 			os.Exit(2)
@@ -50,11 +78,15 @@ func main() {
 		numberOfWorkers = DefaultNumberOfWorkers
 	}
 
+	unlocked := acquireLock(9292)
+	if !unlocked {
+		fmt.Println("Process already running")
+		os.Exit(0)
+	}
+
 	// Start Up access to our listings
 	var homeDb *home.DB
 	homeDb = home.NewDB(dbHost, dbName)
-
-	fmt.Println("We have our home database, now we're gonna iterate..")
 
 	// Make channel, buffered with expected number of workers
 	listingQueue := make(chan home.Listing, numberOfWorkers)
@@ -64,73 +96,57 @@ func main() {
 		fmt.Println("Starting up worker ", i+1)
 		go queueWorker(listingQueue, homeDb)
 	}
+	GlobalWG.Add(numberOfWorkers)
 
-	var count int
-	for {
-		count = 0
-		homeDb.IterateAllListings(func(listing home.Listing, db *home.DB) {
-			listingQueue <- listing
-			count++
-		})
-		fmt.Println("Somehow we're done with all of our listings... starting over again.")
-		fmt.Println("Processed this many: ", count)
-	}
-
+	var count int = 0
+	homeDb.IterateListingsOlderThan(staleDate, batchAmount, func(listing home.Listing, db *home.DB) {
+		listingQueue <- listing
+		count++
+	})
+	fmt.Printf("Finished Queing up: %v listings\n", count)
 
 	// Close Queue
 	close(listingQueue)
 
+	GlobalWG.Wait()
+
+	fmt.Printf("Done - %v\n", time.Now())
+
 }
 
 func queueWorker(queue <-chan home.Listing, db *home.DB) {
+	defer GlobalWG.Done()
 	for listing := range queue {
-		checkAndUpdateMarkup(listing, db)
+		updateListing(listing, db)
 	}
 }
 
-func checkAndUpdateMarkup(listing home.Listing, db *home.DB) {
-
-	// Get the latest date of markup for listing
-	date, found := db.GetNewestMarkupDate(listing.Id)
-
-	// If found and date is not expired, don't continue
-	if found && !isMarkupExpired(date) {
-		//fmt.Println("Current markup record is not expired yet, skipping\n  ->", listing.Url)
-		return
-	}
+func updateListing(listing home.Listing, db *home.DB) {
 
 	// Fetch page
-	//fmt.Println("Making request...")
+	fmt.Println("Making request...")
 	bodyString, fetchErr := fetchPageString(listing.Url)
-	//fmt.Println("Making request...Done")
+	fmt.Println("Making request...Done")
 	if fetchErr != nil {
 		fmt.Printf("Error Fetching Markup\n  -> %s\n  ==> %s\n", listing.Url, fetchErr)
 		// TODO -- should we retry on errors??
 		return
 	}
 
-	// TODO -- is this safe??
-	go func() {
+	// Re-Register with our home db
+	listing, existed, err := db.RegisterListing(listing.Url, listing.Source, bodyString)
+	if err != nil {
+		fmt.Printf("[ERR] Problem re-registering listing: %s - %s\n", listing.Url, err)
+		return
+	}
 
-		// Save Markup
-		saveErr := db.SaveMarkup(listing.Id, listing.Url, listing.Source, bodyString)
+	if !existed {
+		fmt.Printf("[INFO] Somehow listing did not exist, and was inserted: %s - %s\n", listing.Url, listing.Id)
+	}
 
-		if saveErr == nil {
-			bytes := len(bodyString)
-			fmt.Printf("Saved Markup (%v bytes)\n  -> %s\n", bytes, listing.Url)
-		} else {
-			fmt.Printf("Error Saving Markup\n  -> %s\n  ==> %s\n", listing.Url, saveErr)
-			return
-			// TODO -- should we retry on errors??
-		}
+	fmt.Printf("Listing Registered: %+v\n", listing)
+	//fmt.Printf("Listing Registered: (%v) %v\n", listing.Id.Hex(), listing.Url)
 
-	}()
-
-}
-
-func isMarkupExpired(date time.Time) bool {
-	expirationHours := float64(MarkupExpirationDays * 24)
-	return time.Since(date).Hours() > expirationHours
 }
 
 func fetchPageString(uri string) (string, error) {
@@ -166,6 +182,26 @@ func fetchPage(uri string) (io.ReadCloser, error) {
 		return nil, err
 	}
 	return resp.Body, nil
+}
+
+func acquireLock(lockId int) bool {
+
+	// Hacky way to lock this process down to only one process
+	listener, err := net.Listen("tcp", fmt.Sprintf("127.0.0.1:%d", lockId))
+	if err != nil {
+		if strings.Index(err.Error(), "in use") != -1 {
+			return false
+		} else {
+			panic(err)
+		}
+	}
+
+	// TODO - anything else we need to do to keep this open??
+	go func() {
+		listener.Accept()
+	}()
+
+	return true
 }
 
 // ajax image request example
